11702bd17a85fabca01625fad9a0ad764d8f4254
dong53358 <dong53358@naver.com>
2022-04-09 00:45:18 +0900

    day 11

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-let a = 10;
-let b = 2;
-let myName = "gg";
-
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
-
-myName = "gyuyong";
-console.log("my name is " + myName);
+const amIFat = null;
+let something;
+console.log(something, amIFat);

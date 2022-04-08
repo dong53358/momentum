1cb1204847b3f25a2c2c0ea63e2f86d816344fb0
dong53358 <dong53358@naver.com>
2022-04-08 18:55:50 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let a = 10;
+let b = 2;
+let myName = "gg";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+myName = "gyuyong";
+console.log("my name is " + myName);

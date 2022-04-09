3cbad4b84ab8ec48fc1bc23c24b90a297dd7540f
dong53358 <dong53358@naver.com>
2022-04-09 19:03:20 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-const amIFat = null;
-let something;
-console.log(something, amIFat);
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+
+const dayOfWeek = [mon, tue, wed, thu, fri, sat, sun];
+
+const nonsense = [1, 2, "hello", false, null, true, undefined, "gg"];
+
+console.log(dayOfWeek, nonsense);

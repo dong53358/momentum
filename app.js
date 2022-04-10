e520ff736e5c3cc92a7f2454afff4bf091a81610
dong53358 <dong53358@naver.com>
2022-04-11 02:46:04 +0900

    function 2

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-function sayHello(nameOfPerson) {
-  console.log("Hello my name is " + nameOfPerson);
+const player = {
+  name: "gg",
+  sayHello: function (name) {
+    console.log("hello! my name is " + name);
+  },
+};
+
+function plus(firstNumber, secondNumber) {
+  result = firstNumber + secondNumber;
+  console.log(result);
 }
-sayHello("gg");
-sayHello("nico");
-sayHello("aa");
-sayHello("bb");
+
+function divide(a, b) {
+  result = a / b;
+  console.log(result);
+}
+
+plus(8, 90);
+divide(100, 10);
+console.log(player.name);
+
+player.sayHello("gg");

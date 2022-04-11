ca6340e5354e19841a0606c59ffc14408fde584a
dong53358 <dong53358@naver.com>
2022-04-11 20:13:32 +0900

    calculator

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
-const player = {
-  name: "gg",
-  sayHello: function (name) {
-    console.log("hello! my name is " + name);
+const calculator = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+  sub: function (a, b) {
+    console.log(a - b);
+  },
+  dev: function (a, b) {
+    console.log(a / b);
+  },
+  mul: function (a, b) {
+    console.log(a * b);
+  },
+  squ: function (a, b) {
+    console.log(a ** b);
   },
 };
 
-function plus(firstNumber, secondNumber) {
-  result = firstNumber + secondNumber;
-  console.log(result);
-}
-
-function divide(a, b) {
-  result = a / b;
-  console.log(result);
-}
-
-plus(8, 90);
-divide(100, 10);
-console.log(player.name);
-
-player.sayHello("gg");
+calculator.add(100, 10);
+calculator.sub(100, 10);
+calculator.dev(100, 10);
+calculator.mul(100, 10);
+calculator.squ(2, 5);

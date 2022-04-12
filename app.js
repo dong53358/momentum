a38a67b086791b29202946d606bd11f0e476cbc2
dong53358 <dong53358@naver.com>
2022-04-12 16:38:14 +0900

    conditionals 2

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ if (isNaN(age)) {
   console.log("Please wtite a number");
 } else if (age < 18) {
   console.log("You are too young");
-} else if (age > 50) {
-  console.log("You don't drink soju");
-} else {
+} else if (age >= 18 && age <= 50) {
   console.log("You can drink, soju");
+} else {
+  console.log("You don't drink soju");
 }

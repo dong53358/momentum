ff735aa4b2ba9b4293603d05e4a55b966bea426f
dong53358 <dong53358@naver.com>
2022-04-12 16:31:11 +0900

    conditional

--- app.js
+++ app.js
@@ -1,40 +1,11 @@
-const calculator = {
-  add: function (a, b) {
-    return a + b;
-  },
-  sub: function (a, b) {
-    return a - b;
-  },
-  dev: function (a, b) {
-    return a / b;
-  },
-  mul: function (a, b) {
-    return a * b;
-  },
-  squ: function (a, b) {
-    return a ** b;
-  },
-};
+const age = parseInt(prompt("How old are you?"));
 
-const plusResult = calculator.add(2, 3);
-const subResult = calculator.sub(plusResult, 10);
-const devResult = calculator.dev(10, subResult);
-const mulResult = calculator.mul(devResult, plusResult);
-const squResult = calculator.squ(mulResult, subResult);
-
-const result = [plusResult, subResult, devResult, mulResult, squResult];
-
-for (i = 0; i < result.length; i++) {
-  console.log(result[i]);
+if (isNaN(age)) {
+  console.log("Please wtite a number");
+} else if (age < 18) {
+  console.log("You are too young");
+} else if (age > 50) {
+  console.log("You don't drink soju");
+} else {
+  console.log("You can drink, soju");
 }
-
-let age = prompt("How old are you?");
-
-console.log(age);
-console.log(typeof age);
-age = parseInt(age);
-console.log(typeof age);
-
-age = parseInt(prompt("How old are you?"));
-
-console.log(age);

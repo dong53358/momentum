90f21d21fa412036f0e5bc78d91330df38b9a83b
dong53358 <dong53358@naver.com>
2022-04-12 23:49:44 +0900

    querySector

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
-const age = parseInt(prompt("How old are you?"));
+const title = document.querySelector(".hello h1");
 
-if (isNaN(age) || age < 0) {
-  console.log("Please wtite a real positive number");
-} else if (age < 18) {
-  console.log("You are too young");
-} else if (age >= 18 && age <= 50) {
-  console.log("You can drink, soju");
-} else if (age >= 50 && age <= 80) {
-  console.log("You should exercisd");
-} else if (age > 80) {
-  console.log("You can do whatever you want");
-}
+title.innerText = "Hello!";
+
+console.log(title);

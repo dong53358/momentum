7354575a19e09b3c086da97f139f6245d12177b5
dong53358 <dong53358@naver.com>
2022-04-17 20:22:26 +0900

    form update

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+const loginForm = document.querySelector(".login-form");
 const loginInput = document.querySelector(".login-form input");
 
-function handleLoginBtnClick() {
+function onLoginSubmit() {
   const username = loginInput.value;
   console.log(username);
 }
+loginForm.addEventListener("submit", onLoginSubmit);

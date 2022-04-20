18ef725fd35e5be88ae85ef32b53a2a4faa9c0d7
dong53358 <dong53358@naver.com>
2022-04-20 19:42:04 +0900

    clock end

---
+++ js/clock.js
@@ -0,0 +1,15 @@
+const clock = document.querySelector("#clock");
+
+function getClock() {
+  let date = new Date();
+  let hours = String(date.getHours()).padStart(2, "0");
+  let minutes = String(date.getMinutes()).padStart(2, "0");
+  let seconds = String(date.getSeconds()).padStart(2, "0");
+
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+
+  // clock.innerText = new Date().toLocaleTimeString();
+}
+
+getClock();
+setInterval(getClock, 1000);
